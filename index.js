90eb721cf78a53fcadf796ab0a9b1b9dba4b0d8d
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-15 22:13:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-// Your code here
+const createEmployeeRecord = (arr) => {
+  return {
+      firstName : arr[0],
+      familyName : arr[1],
+      title : arr[2],
+      payPerHour: arr[3],
+      timeInEvents : [],
+      timeOutEvents : []
+    };
+};
+
+const createEmployeeRecords = (matrix) => {
+ let result = [];
+  let s = matrix.map(arr => {
+    return result.push(createEmployeeRecord(arr));
+  });
+ return result;
+  };
+  
+const createTimeInEvent = (arr, dateIn) => {
+  let dateArray = dateIn.split(' ');
+  let timeObject = {
+    type: "TimeIn",
+    date: dateArray[0],
+    hour: parseInt(dateArray[1])
+  };
+  arr.timeInEvents.push(timeObject);
+  return arr;
+};
+   
+const createTimeOutEvent = (arr, dateOut) => {
+   let dateArray = dateOut.split(' ');
+  let timeObject = {
+    type: "TimeOut",
+    date: dateArray[0],
+    hour: parseInt(dateArray[1])
+  };
+      arr.timeOutEvents.push(timeObject);
+      return arr;
+};
+   
+const hoursWorkedOnDate = (arr, date=0) => {
+  let inEvent = arr.timeInEvents.find(function(e){
+        return e.date === date
+    })
+
+    let outEvent = arr.timeOutEvents.find(function(e){
+        return e.date === date
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+  }
+
+
+const wagesEarnedOnDate = (arr, date=0) => {
+    let hoursWorked = hoursWorkedOnDate(arr, date)
+    let wages = arr.payPerHour*hoursWorked
+    return wages
+}
+
+const allWagesFor = (arr) =>{
+  let eligibleDates = arr.timeInEvents.map(el => {
+        return el.date
+    })
+
+    let payable = eligibleDates.reduce((memo, d) => {
+        return memo + wagesEarnedOnDate(arr, d)
+    }, 0)
+
+    return payable
+}
+const calculatePayroll = (arr) => {
+    return arr.reduce((acc, val) => {
+        return acc + allWagesFor(val)
+    }, 0)
+}  
+
+const findEmployeeByFirstName = (arr, string) => {
+  return arr.find(el => {
+    return el.firstName === string
+  })
+}  
+
